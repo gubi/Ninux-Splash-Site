856de1613ecd74151c8b9805a89d1ff31bf51b7b
Alessandro <gubi.ale@gotanotherway.com>
2012-05-08 14:16:58 +0200

    Initial files import

---
+++ common/js/pages.js
@@ -0,0 +1,51 @@
+/*
+
+=> Content recall file
+
+Please fill out the array with page data and create a tpl file in "common/tpl/" for a new page
+Suggestion: in tpl file backslash the quotation mark for a better content loading
+*/
+var pages = {
+	"home": {
+		id: "home",
+		page: "",
+		title: "Benvenut* in Ninux!",
+		body: "home.tpl"
+	},
+	"manifesto": {
+		id: "manifesto",
+		page: "Manifesto",
+		title: "Pagina del Manifesto",
+		body: "manifesto.tpl"
+	},
+	"wcm": {
+		id: "wcm",
+		page: "Wireless Community Manifesto",
+		title: "Wireless Community Manifesto",
+		body: "wcm.tpl"
+	},
+	"la_rete": {
+		id: "la_rete",
+		page: "La Rete",
+		title: "La Rete",
+		body: "la_rete.tpl"
+	},
+	"cn": {
+		id: "cn",
+		page: "Community Network",
+		title: "Community Network",
+		body: "cn.tpl"
+	},
+	"ppa": {
+		id: "ppa",
+		page: "Picopeering Agreement",
+		title: "Picopeering Agreement",
+		body: "ppa.tpl"
+	},
+	"faq": {
+		id: "faq",
+		page: "FAQ",
+		title: "Frequently Asked Questions",
+		body: "faq.tpl"
+	}
+};
